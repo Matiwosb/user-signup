bfde6092116966b17c6b6157f9cbddbc9fa54080
Matiwos Birbo <workubirbo@gmail.com>
2019-05-22 04:04:32 -0500

    added template and the error shows up now

--- main.py
+++ main.py
@@ -1,39 +1,24 @@
 from flask import Flask, request, render_template,redirect
 import cgi
 import os
+import jinja2
 import re
+
+template_dir = os.path.join(os.path.dirname(__file__), 'temaplates')
+jinja2_env = jinja2.Environment(loader = jinja2.FileSystemLoader
+(template_dir), autoescape=True)
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-page_header = """
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-5" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Validation Example</title>
-    <link rel="stylesheet" href="/static/app.css" />
-  </head>
-  <body>
-"""
-
-welcomeMessage = """
-<h1>Welcome to my super cool page!</h1>
-<a href="/signup">Register</a> """
-
-
-page_footer = """
- </body>
-</html>
-"""
-
 # a registration form
 register_form = """
-<form action="/signup" id="form" method="POST">
-      <h1>Register</h1>
+    <style>
+        .error{{color: red;}}
+    </style>
+    <form action="/signup" id="form" method="POST">
+      <h1>Signup</h1>
       <label for="username">Username </label>
       <input type="text" name="username" id="username" value="{username}" />
       <p class="error">{usernameError}</p>
@@ -46,7 +31,7 @@ register_form = """
       <label for="email">Email(optional)</label>
       <input type="text" name="email" id="email" value="{email}"/>
       <p class="error">{emailError}</p>
-      <button type="submit">Register</button>
+      <button type="submit">Submit</button>
     </form>
 """
 @app.route('/signup')
@@ -81,13 +66,7 @@ def register():
         passwordError = "Password must be at least 3 to 20 characters long."
     elif " " in password:
         passwordError = "Your cannot contain any spaces"
-    '''else:
-        hasNumber = False
-        for char in password:
-            if char.isdigit():
-                hasNumber = True
-        if not hasNumber:
-            passwordError = "Password must contain a number"'''
+    
     if verify_password == "" or verify_password != password:
         verify_passwordError = "Password do not match. Please try again."
         verify_password = ""
@@ -97,25 +76,11 @@ def register():
     
 
     if usernameError or passwordError or verify_passwordError or emailError :
-        return register_form.format(username, usernameError, 
-        password, passwordError, verify_password, verify_passwordError, email, emailError)
-        '''print("there was an error!")
-        content = page_header + register_form.format(username, usernameError, 
-        password, passwordError, verify_password, verify_passwordError, email, emailError) + page_footer
-        return content'''
-    return "Welcome, " + username
-
-
-@app.route("/")
-def index():
-    # build the response string
-    content = page_header + welcomeMessage + page_footer
-    return content
-
-@app.route("/signup", methods=['GET'])
-def register_page():
-    # build the response string
-    content = page_header + register_form.format("", "", "", "", "", "","","") + page_footer
-    return content
+        return register_form.format(username=username, usernameError=usernameError, 
+        password=password, passwordError=passwordError, verify_password=verify_password, 
+        verify_passwordError=verify_passwordError, email=email, emailError=emailError)
+    '''template = jinja2_env.get_template("Welcome.html")
+    return template.render()'''
+    return '<h1>Welcome, ' + username + ' !</h1>'
 
 app.run()
